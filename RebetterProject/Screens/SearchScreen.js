e771f51d1ef8282b650d63f6fe2d53875a0828af
mcy0325 <mcy325@naver.com>
2024-05-27 22:23:24 +0900

    0527 naver map

--- RebetterProject/Screens/SearchScreen.js
+++ RebetterProject/Screens/SearchScreen.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  Image,
-  ImageBackground,
+  TextInput,
+  Alert,
 } from 'react-native';
 import {colors} from '../theme';
+import {
+  NaverMapView,
+  NaverMapMarkerOverlay,
+} from '@mj-studio/react-native-naver-map';
 
 const SearchScreen = ({navigation: {navigate}}) => {
-  return <View style={styles.Container}></View>;
+  return (
+    <View style={{flex: 1}}>
+      <NaverMapView
+        style={{flex: 1}}
+        initialCamera={{
+          latitude: 37.5992142,
+          longitude: 126.8652116,
+        }}
+      />
+    </View>
+  );
 };
 
 export default SearchScreen;
-
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    paddingHorizontal: '5%',
-  },
-});
